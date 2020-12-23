942d58baacebc3cc8e211872c3126a1f3ac326ef
Ubuntu <ubuntu@ip-172-31-43-226.us-east-2.compute.internal>
2020-12-23 12:53:07 +0000

    .

--- view_cntr/views.py
+++ view_cntr/views.py
@@ -3,6 +3,26 @@ from .models import *
 
 def dashboard(request):
 	args={}
+	if request.method == 'POST':
+		price_min = request.POST['price_min']
+		price_max = request.POST['price_max']
+		width = request.POST['width']
+		height = request.POST['height']
+		length = request.POST['length']
+		weight = request.POST['weight']
+
+		args['price_min'] = price_min
+		args['price_max'] = price_max
+		args['width'] = width
+		args['height'] = height
+		args['length'] = length
+		args['weight'] = weight
+
+		products = Product.objects.all()
+		# products = products.order_by('-count')[0:50]
+		products = products[0:50]
+		args['products'] = products
+
 	return render(request, 'view_cntr/dashboard.html', args)
 
 def view_cntr(request):
